b5a91f7abe9f78d580767aafdb57d6abc8b4886b
Sumit Nikam <sumitnikam03@gmail.com>
2024-10-21 14:26:49 +0530

    For Live

--- bankServer/index.js
+++ bankServer/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {PORT} from "./Utility/constants.js";
+import { PORT } from "./Utility/constants.js";
 import { dbConnection } from "./Utility/dbConnection.js";
 import { customerRouter } from "./Routes/customerRoutes.js";
 import { adminRouter } from "./Routes/adminRoutes.js";
@@ -8,22 +8,26 @@ import cors from 'cors'
 const app = express();
 app.use(cors())
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000', // Adjust based on your frontend's origin
+    methods: ['GET', 'POST'], // Add other methods if needed
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use('/customers', customerRouter)
+app.use('/admin', adminRouter)
 
-app.use('/customers',customerRouter)
-app.use('/admin',adminRouter)
-
-app.use('/',(req,res)=>{res.send('Welcome to RuPay Bank Server')})
+app.use('/', (req, res) => { res.send('Welcome to RuPay Bank Server') })
 
 app.listen(PORT, () => {
-    dbConnection.connect(error=>{
-        if(error){
+    dbConnection.connect(error => {
+        if (error) {
             console.log("Error while connecting Db");
             // console.log(error)
-        }else{
+        } else {
             console.log("Db connected Successfully...!")
         }
     })
-  console.log(`\nServer is listening on port ${PORT}\n`);
-  console.log(`Local            : http://localhost:${PORT}\n`);
-  console.log(`On Your Network  : http://192.168.0.107:${PORT}\n`)
+    console.log(`\nServer is listening on port ${PORT}\n`);
+    console.log(`Local            : http://localhost:${PORT}\n`);
+    console.log(`On Your Network  : http://192.168.0.107:${PORT}\n`)
 });
